2aabdb89ea39bfbcc54bdd1b10f50294a0520128
Min-ho Kim <xho1995@gmail.com>
2020-12-16 12:52:36 +0900

    makes the PatternStyle functions public

--- Sources/ShapeKit/PatternStyle.swift
+++ Sources/ShapeKit/PatternStyle.swift
@@ -9,28 +9,28 @@ import SwiftUI
 
 public struct PatternStyle {
     @available(iOS 13.0, *)
-    static func stripe(color: Color = Color.white,
+    public static func stripe(color: Color = Color.white,
                        width: CGFloat = 5.0,
                        spacing: CGFloat = 5.0) -> some Pattern {
         StripePattern(color: color, width: width, spacing: spacing)
     }
 
     @available(iOS 13.0, *)
-    static func dot(color: Color = Color.white,
+    public static func dot(color: Color = Color.white,
                        width: CGFloat = 5.0,
                        spacing: CGFloat = 5.0) -> some Pattern {
         DotPattern(color: color, width: width, spacing: spacing)
     }
 
     @available(iOS 13.0, *)
-    static func check(color: Color = Color.white.opacity(0.5),
+    public static func check(color: Color = Color.white.opacity(0.5),
                        width: CGFloat = 5.0,
                        spacing: CGFloat = 5.0) -> some Pattern {
         CheckPattern(color: color, width: width, spacing: spacing)
     }
 
     @available(iOS 13.0, *)
-    static func check(first: PatternConfig,
+    public static func check(first: PatternConfig,
                       second: PatternConfig) -> some Pattern {
         CheckPattern(first: first, second: second)
     }
