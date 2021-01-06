401307d91793bd408fd403aab9410a8cfc65fb78
Min-ho Kim <xho1995@gmail.com>
2021-01-06 18:49:11 +0900

    changes the manifest file

--- Package.swift
+++ Package.swift
@@ -12,29 +12,25 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ShapeKit",
-            targets: ["ShapeKit"]),
+            targets: ["ShapeKit", "CoreShape"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        //.package(url: "https://github.com/xho95/CoreShape.git", from: "0.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ShapeKit",
-            dependencies: [
-                .target(name: "CoreShape", condition: .when(platforms: [.iOS])),
-            ]),
+            dependencies: ["CoreShape"]),
         .binaryTarget(
             name: "CoreShape",
-            url: "https://github.com/xho95/CoreShape.zip",
-            checksum: "checksum"),
-
-        // While building for macOS, no library for this platform was found in '/Users/kimminho/Library/Developer/Xcode/DerivedData/ShapeKit-ghwawczntlfoajbscwcyubvzltdb/SourcePackages/checkouts/CoreShape/CoreShape.xcframework'.
-
+            url: "https://github.com/xho95/CoreShape/releases/tag/0.0.1/CoreShape-0.0.1.xcframework.zip",
+            checksum: "47e86e3bd5edec935b5bb634ef4ff22f7dcb27259b05f6a88bf5aab6ceadc6c7"),
         .testTarget(
             name: "ShapeKitTests",
-            dependencies: ["ShapeKit"]),
+            dependencies: ["ShapeKit", "CoreShape"]),
     ]
 )
