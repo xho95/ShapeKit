31a93d457af0b098c05f37601e7355c904b3dd10
Min-ho Kim <xho1995@gmail.com>
2021-01-07 00:26:46 +0900

    modifies the package file

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ShapeKit",
-            targets: ["ShapeKit", "CoreShape"]),
+            targets: ["ShapeKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        //.package(url: "https://github.com/xho95/CoreShape.git", from: "0.0.1")
+        // .package(url: "https://github.com/xho95/CoreShape.git", from: "0.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,10 +27,10 @@ let package = Package(
             dependencies: ["CoreShape"]),
         .binaryTarget(
             name: "CoreShape",
-            url: "https://github.com/xho95/CoreShape/releases/tag/0.0.1/CoreShape-0.0.1.xcframework.zip",
+            url: "https://github.com/xho95/CoreShape/releases/download/0.0.1/CoreShape-0.0.1.xcframework.zip",
             checksum: "47e86e3bd5edec935b5bb634ef4ff22f7dcb27259b05f6a88bf5aab6ceadc6c7"),
         .testTarget(
             name: "ShapeKitTests",
-            dependencies: ["ShapeKit", "CoreShape"]),
+            dependencies: ["ShapeKit"]),
     ]
 )
